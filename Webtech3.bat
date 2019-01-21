e5fd5da913e1f079278d5cdc3f567274602353f3
JanssensJordy <43535431+JanssensJordy@users.noreply.github.com>
2019-01-21 12:40:54 +0100

    Add files via upload
    Fixed a bug where the different directory didn't work.

--- Webtech3.bat
+++ Webtech3.bat
@@ -12,10 +12,10 @@ echo.
 echo ###################################################
 echo.
 
-CHOICE /C ynq /N /M "Install in this folder(Y/N)? or Quit(Q)?"
+CHOICE /C nyq /N /M "Install in this folder(Y/N)? or Quit(Q)?"
 IF ERRORLEVEL 3 goto end
-IF ERRORLEVEL 2 echo Pad: & SET /p PAD=
-IF ERRORLEVEL 1 SET PAD="%~dp0"
+IF ERRORLEVEL 2 SET PAD="%~dp0"
+IF ERRORLEVEL 1 echo Pad: & SET /p PAD=
 echo.
 
 CHOICE /C EAQ /N /M "Install Express(E) / Angular(A) or Quit(Q)?"
@@ -25,11 +25,11 @@ IF ERRORLEVEL 1 goto express
 
 
 :angular
-start cmd /k "TITLE Angular ng serve of npm run serve & cd "%angularDir%" & npm install & npm install mongodb --save & npm install -g @angular/cli & npm install body-parser --save & npm install cors --save"
+start cmd /k "TITLE Angular ng serve of npm run serve & cd "%PAD%" & npm install & npm install mongodb --save & npm install -g @angular/cli & npm install body-parser --save & npm install cors --save"
 goto start
 
 :express
-start cmd /k "TITLE EXPRESS node server & cd "%expressDir%" & npm install & npm install express --save & npm install body-parser --save & npm install mongodb --save & npm install ejs --save & npm install nodemon --save-dev"
+start cmd /k "TITLE EXPRESS node server & cd "%PAD%" & npm install & npm install express --save & npm install body-parser --save & npm install mongodb --save & npm install ejs --save & npm install nodemon --save-dev"
 goto start
 
 :end
